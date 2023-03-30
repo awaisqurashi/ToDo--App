85085f1aa2cca47e0c8b53204934a4f5cb074873
awaisqurashi <103379729+awaisqurashi@users.noreply.github.com>
2023-03-30 18:18:13 +0500

    Todo app finalize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
 
 function App() {
+
+  const [inputValue, SetinputValue] = useState() 
+  const [mainArray, setmainArray ] =useState([])
+ 
+const [editCheck, setEditCheck] =useState(false)
+
+const [editValue, SeteditValue] = useState("")
+
+function addFunc(){
+  if(editCheck){
+
+    mainArray[editValue] = inputValue
+    setmainArray(mainArray)
+    setEditCheck(false)
+  }else{
+    setmainArray([...mainArray, inputValue])
+  }
+
+
+
+  SetinputValue("")
+ }
+
+ function deleteFunc(index){
+  let tempArray =[...mainArray]
+  tempArray.splice(index,1)
+setmainArray(tempArray)
+ }
+
+ function editFunc(index){ 
+SetinputValue(mainArray[index])
+SeteditValue (index)
+setEditCheck(true)
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <input onChange={(e)=>{SetinputValue(e.target.value)}} value={inputValue} type="text"/>
+      <button onClick={addFunc}>Add</button>
+    <div>
+      <ul>
+        {mainArray.map((item,index)=>{
+          return <li key={index}>{item}
+          <button onClick={()=>{deleteFunc(index)}}>Delete</button>
+          <button onClick={()=>{editFunc(index)}}>Edit</button></li>
+        })}
+      </ul>
     </div>
-  );
+    
+    </div>
+
+    </>
+  )
 }
 
-export default App;
+export default App
